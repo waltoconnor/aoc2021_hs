9cf4a130646f93fe9e38854f404a41019bea12f4
Walt <woconno2@uoregon.edu>
2022-11-21 14:52:19 -0800

    Day 2

---
+++ day2/control_aim.hs
@@ -0,0 +1,29 @@
+import System.IO (openFile, IOMode (ReadMode), hGetContents)
+main :: IO()
+
+main = do 
+    input_commands <- readInput
+    let deltas = map commandToDelta input_commands
+    print deltas
+    let final_position = foldl sumDelta (0, 0, 0) deltas
+
+    let (x, y, aim) = final_position in print (x * y)
+
+
+readInput :: IO [(String, Int)]
+readInput = do 
+    handle <- openFile "input.txt" ReadMode
+    input <- hGetContents handle
+    let input_lines = lines input
+    let v = map (\l -> let [cmd, num] = words l in (cmd, read num)) input_lines
+    return v
+
+commandToDelta :: (String, Int) -> (Int, Int, Int)
+commandToDelta ("forward", x) = (x, 0, 0)
+commandToDelta ("down", y) = (0, 0, y)
+commandToDelta ("up", y) = (0, 0, -y)
+commandToDelta(_, x) = (0, 0, 0)
+
+
+sumDelta :: (Int, Int, Int) -> (Int, Int, Int) -> (Int, Int, Int)
+sumDelta (x_pos,y_pos,aim) (x_delta, y_delta, aim_delta) = (x_delta + x_pos, y_pos + (x_delta * aim), aim + aim_delta)
